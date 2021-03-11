fd654bd54b0d797015cc408906984f1c0b1c97ef
Gabriel <gabriel.jchen@gmail.com>
2021-03-11 22:07:31 +0000

    refactored with lots of help from mentors

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,34 +1,29 @@
-const arg = process.argv.slice(2);
-const fs = require('fs');
 const { stdout } = require('process');
 const request = require('request');
 
-
-const urlString = "https://api.thecatapi.com/v1/breeds/search?q=" + (arg);
-
-// console.log(urlString);
-
-request(urlString, (error, response, body) => {
-  console.log('error:', error);
-  console.log('statusCode:', response && response.statusCode);
-  // console.log('body:', body.length);
-  // console.log("TYPE OF BODY--->" ,typeof body);
-  // fs.writeFile(arg[1], body, 'utf8', () => {
-  //   stdout.write(` ${body} \n`)
-  // })
-  const data = JSON.parse(body);
-  const catDescription = data[0];
-  console.log("cat description-->", catDescription);
+const fetchBreedDescription = function(breedName, callback) {
+const urlString = "https://api.thecatapi.com/v1/breeds/search?q=" + (breedName);
   
-  if (!catDescription) {
-    stdout.write(`Error: ${error} `);
-  } else {
-    stdout.write(`Description: ${catDescription.description} \n`);
-  }
-});
+  
+  request(urlString, (error, response, body) => {
+   
+    if (error) {
+      callback(error, null);
+      return;
+    }
+      
+    const data = JSON.parse(body);
+    const catDescription = data[0];
+        
+    if (!catDescription) {
+      callback("Breed not found", null);
+    } else {
+      callback(null, catDescription.description);
+    }
+  });
+  
+};
 
-// console.log('args -->', arg);
 
-///////////
-// use fs.writeFile() to write file
-// return "Downloaded and saved x bytes to ./index.html" on the command line.
+
+module.exports = { fetchBreedDescription }
